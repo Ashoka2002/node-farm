5792f781717ae4d6c7d694aed2b146e6745d0517
Ashok Choudhary <ashoka8769@gmail.com>
2023-12-15 09:33:52 +0530

    product overive

--- index.js
+++ index.js
@@ -29,26 +29,65 @@ const { json } = require("stream/consumers");
 ////////////////////////////////////////////
 //SERVER
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 const dataJson = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(dataJson);
+
+function replaceTemplate(temp, product) {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+
+  return output;
+}
 
 const server = http.createServer((req, res) => {
+  //OVERVIEW PAGE
   if (req.url === "/overview" || req.url === "/") {
     res.writeHead(200, {
       "Content-type": "text/html",
-      "Who-is-kiran": "my-wife",
     });
-    res.end("<h1 style='color:green;'>This is OVERVIEW</h1>");
-  } else if (req.url === "/api") {
-    res.writeHead(200, {
-      "Content-type": "application/json",
-    });
-    res.end(dataJson);
+
+    const cardHTML = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardHTML);
+    res.end(output);
+
+    //PRODUCT PAGE
   } else if (req.url === "/product") {
     res.writeHead(200, {
       "Content-type": "text/html",
       "Who-is-kiran": "my-wife",
     });
     res.end("<h1 style='color:green;'>This is PRODUCT</h1>");
+
+    //API PAGE
+  } else if (req.url === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(dataJson);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
